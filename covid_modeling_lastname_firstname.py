89c965afc2559a6633464df17167a0d8e2f25193
Ryan Kelley <kelleyr@duvalschools.org>
2020-04-07 12:20:22 -0400

    Version 0.1
    Added variables for number of infected, days to simulate, number of deaths.

---
+++ covid_modeling_lastname_firstname.py
@@ -0,0 +1,32 @@
+# Simple COVID-19 Exponential Growth Simulator Template -- Ryan K. - 04/07/2020, 12:11PM Version 0.1
+
+import time
+
+num_infected = 0  
+num_days = 0
+num_simulate = 0
+num_deaths = 0 
+
+# Use a print() statement to explain that the number of infections doubles every six days.
+
+# Use time.sleep() to pause a few seconds so the user can read your print() statement.
+
+# Use an input() statement to have the user enter the CURRENT NUMBER OF INFECTED CASES.  Make sure it's an INT.
+# Use an input() statement to have the user enter the NUMBER OF DAYS TO SIMULATE.  Make sure it's an INT.
+
+# Use a print() statement to display the current number of infected people and how many days to simulate.
+
+# Remove the comment on the next line and change the two variables to reflect the variable names you used. 
+# while VARIABLE FOR DAY NUMBER <= NUMBER OF DAYS TO SIMULATE:
+    
+    # Write an assignment statement (use =) to double the number of infected people. 
+    # Write an assignment statement (use =) to multiply the number of infected people by the mortality rate to find the number of deaths.
+    # Write a print() statement that displays the number of infected people and the number of deaths. 
+    # Increment the current day number by 6, since the number of infections doubles every six days approximately.  
+    # Use time.sleep() to pause for a few seconds to allow the user to read the instructions.  
+
+# Write a print() statement that shows the total number of infections and the number of deaths after running your simulation.  
+
+    
+    
+    

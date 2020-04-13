2da23d23d96bfda87ab14022ef55b70d6e73247a
Ryan Kelley <kelleyr@duvalschools.org>
2020-04-13 10:20:27 -0400

    Version 0.0
    Starting the program.

---
+++ kelley_ryan_covid_modeling.py
@@ -0,0 +1,32 @@
+# Simple COVID-19 Exponential Growth Simulator Template -- YRyan K.  -- 04/13/20 -- 10:19am -- Version 0.0
+
+import time
+
+# Create a variable here that will store the number of infected people, assign it a value of 0.  
+# Create a variable here that will store the current day number, assign it a value of 0. (i.e. the first day is day 0, the second day is day 1, etc.)
+# Create a variable here that will store the total number of days to simluate, assign it a value of 0. (i.e. how many days worth of growth will you simulate?)
+# Create a variable here that will store the number of people that will die from COVID-19, assign it a value of 0.  
+
+# Use a print() statement to explain that the number of infections doubles every six days.
+
+# Use time.sleep() to pause a few seconds so the user can read your print() statement.
+
+# Use an input() statement to have the user enter the CURRENT NUMBER OF INFECTED CASES.  Make sure it's an INT.
+# Use an input() statement to have the user enter the NUMBER OF DAYS TO SIMULATE.  Make sure it's an INT.
+
+# Use a print() statement to display the current number of infected people and how many days to simulate.
+
+# Remove the comment on the next line and change the two variables to reflect the variable names you used. 
+# while VARIABLE FOR DAY NUMBER <= NUMBER OF DAYS TO SIMULATE:
+    
+    # Write an assignment statement (use =) to double the number of infected people. 
+    # Write an assignment statement (use =) to multiply the number of infected people by the mortality rate to find the number of deaths.
+    # Write a print() statement that displays the number of infected people and the number of deaths. 
+    # Increment the current day number by 6, since the number of infections doubles every six days approximately.  
+    # Use time.sleep() to pause for a few seconds to allow the user to read the instructions.  
+
+# Write a print() statement that shows the total number of infections and the number of deaths after running your simulation.  
+
+    
+    
+    

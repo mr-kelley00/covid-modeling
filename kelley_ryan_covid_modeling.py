7c6fafc90bc5ee7421d39c0a628c64e6297ea043
Ryan Kelley <kelleyr@duvalschools.org>
2020-04-13 11:44:48 -0400

    Version 0.7a
    Fixed an error with logic,. needed to increase the day number BEFORE calculating growth and deaths.

--- kelley_ryan_covid_modeling.py
+++ kelley_ryan_covid_modeling.py
@@ -1,4 +1,4 @@
-# Simple COVID-19 Exponential Growth Simulator Template -- Ryan K.  -- 04/13/20 -- 11:38am -- Version 0.7
+# Simple COVID-19 Exponential Growth Simulator Template -- Ryan K.  -- 04/13/20 -- 11:44am -- Version 0.7a
 
 import time
 
@@ -18,10 +18,10 @@ print(f"The current number of people infected with COVID-19 is {num_infected_ppl
 
 while current_day <= days_sim:
     
+    current_day += 6
     num_infected_ppl += num_infected_ppl
     num_deaths_ppl = round(num_infected_ppl * 0.023)
     print(f"On day {current_day} there will be {num_infected_ppl:,} and approximately {num_deaths_ppl:,} people will die from COVID-19.\n")
-    current_day += 6  
     time.sleep(3)
 
 # Write a print() statement that shows the total number of infections and the number of deaths after running your simulation.  

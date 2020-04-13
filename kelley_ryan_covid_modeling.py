4d789c99c252ceeab81a80a75bfc7f398c256250
Ryan Kelley <kelleyr@duvalschools.org>
2020-04-13 10:29:19 -0400

    Version 0.1
    Added variables for number of infections, days to simulate, current day, and the number of deaths.

--- kelley_ryan_covid_modeling.py
+++ kelley_ryan_covid_modeling.py
@@ -1,11 +1,11 @@
-# Simple COVID-19 Exponential Growth Simulator Template -- YRyan K.  -- 04/13/20 -- 10:19am -- Version 0.0
+# Simple COVID-19 Exponential Growth Simulator Template -- Ryan K.  -- 04/13/20 -- 10:28am -- Version 0.1
 
 import time
 
-# Create a variable here that will store the number of infected people, assign it a value of 0.  
-# Create a variable here that will store the current day number, assign it a value of 0. (i.e. the first day is day 0, the second day is day 1, etc.)
-# Create a variable here that will store the total number of days to simluate, assign it a value of 0. (i.e. how many days worth of growth will you simulate?)
-# Create a variable here that will store the number of people that will die from COVID-19, assign it a value of 0.  
+num_infected_ppl = 0 
+current_day = 0
+days_sim = 0 
+num_deaths_ppl = 0 
 
 # Use a print() statement to explain that the number of infections doubles every six days.
 
